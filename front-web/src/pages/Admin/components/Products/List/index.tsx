2b5779d359662b1b629b3cd92328bce480b04dc7
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2021-03-18 19:25:12 -0300

    Add products categories

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom'
 import Card from '../Card';
 import './styles.scss';
 
 const List = () => {
+  const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [activePage, setActivePage] = useState(0);
   const history = useHistory();
+  
+  console.log(productsResponse);
+
+  useEffect(() => {
+    const params = {
+      page: activePage,
+      linesPerPage: 12
+    }
+
+    setIsLoading(true);
+    makeRequest({ url: '/products', params })
+      .then(response => setProductsResponse(response.data))
+      .finally(() => setIsLoading(false));
+  }, [activePage]);
 
   const handleCreate = () => {
     history.push('/admin/products/create')
